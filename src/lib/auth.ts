845900126a0b04a9aaa5792360acfd08feb9286a
MonsteRico <boilermonster0530@yahoo.com>
2025-05-05 20:22:04 -0400

    pretty

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,15 +2,15 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/server/db"; // your drizzle instance
 import { env } from "@/env";
- 
+
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
-    }),
-    socialProviders: {
-        discord: {
-            clientId: env.DISCORD_CLIENT_ID as string,
-            clientSecret: env.DISCORD_CLIENT_SECRET as string
-        }
-    }
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+  }),
+  socialProviders: {
+    discord: {
+      clientId: env.DISCORD_CLIENT_ID as string,
+      clientSecret: env.DISCORD_CLIENT_SECRET as string,
+    },
+  },
 });
